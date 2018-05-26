4b581528884bec6a1434af18eb96647bb45f5888
huihui.zhang <glorydom@163.com>
2018-05-27 01:49:28 +0800

    调试环境

--- server/src/main/java/com/dto/huiyi/meeting/util/Constants.java
+++ server/src/main/java/com/dto/huiyi/meeting/util/Constants.java
@@ -1,7 +1,11 @@
 package com.dto.huiyi.meeting.util;
 
+import com.zheng.common.util.PropertiesFileUtil;
+
 public class Constants {
-    public static final String CHQSURL = "http://localhost:8080/chunhuaqiushi/";
+    public static final String CHQSURL = PropertiesFileUtil.getInstance().get("chqsurl");
+//            "http://localhost:8080/chunhuaqiushi/";
+
 
 
     public static final int SUCCESS_CODE = 200;
