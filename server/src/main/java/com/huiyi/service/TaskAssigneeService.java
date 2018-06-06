e60bc83dbe3281e30c09fa9662723af1e8878cd6
harper <haibo1047@gmail.com>
2018-06-07 00:39:02 +0800

    根据taskid返回用户名列表

--- server/src/main/java/com/huiyi/service/TaskAssigneeService.java
+++ server/src/main/java/com/huiyi/service/TaskAssigneeService.java
@@ -1,20 +1,38 @@
 package com.huiyi.service;
 
-import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.huicong.upms.dao.model.UpmsUser;
+import com.huicong.upms.rpc.api.UpmsUserService;
+import com.huiyi.meeting.dao.model.MeetingTaskCandidate;
+import com.huiyi.meeting.dao.model.MeetingTaskCandidateExample;
+import com.huiyi.meeting.rpc.api.MeetingTaskCandidateService;
 
 @Service("TaskAssigneeService")
 public class TaskAssigneeService {
 
+	@Autowired
+	MeetingTaskCandidateService meetingTaskCandidateService;
+	@Autowired
+	UpmsUserService upmsUserService;
     /**
      * 根据任务id返回任务执行人
      * @param taskId
      * @return
      */
     public List<String> getUserIdsByTaskID (String taskId){
-        List list =  new ArrayList();
-        list.add("姚晶");
+    	MeetingTaskCandidateExample example = new MeetingTaskCandidateExample();
+    	example.createCriteria().andTaskidEqualTo(taskId);
+    	List<MeetingTaskCandidate> mtcList = meetingTaskCandidateService.selectByExample(example);
+    	List<String> list =  new ArrayList<>();
+    	for(MeetingTaskCandidate mtc : mtcList) {
+    		UpmsUser user = upmsUserService.selectByPrimaryKey(mtc.getUserid());
+    		list.add(user.getUsername());
+    	}
         return list;
     }
 
