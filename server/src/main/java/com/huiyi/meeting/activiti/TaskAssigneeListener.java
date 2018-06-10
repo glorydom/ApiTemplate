d424f5dc2d5a4ca23ac78ab1d3f9620613efaf6a
harper <haibo1047@gmail.com>
2018-06-10 14:56:22 +0800

    根据任务配置信息 指定任务执行人

--- server/src/main/java/com/huiyi/meeting/activiti/TaskAssigneeListener.java
+++ server/src/main/java/com/huiyi/meeting/activiti/TaskAssigneeListener.java
@@ -1,29 +1,31 @@
 package com.huiyi.meeting.activiti;
 
-import com.huiyi.service.TaskAssigneeService;
-import org.activiti.engine.delegate.DelegateTask;
-import org.activiti.engine.delegate.TaskListener;
-import org.springframework.context.ApplicationContext;
-import org.springframework.web.context.ContextLoader;
-
 import java.io.Serializable;
 import java.util.List;
 
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.ContextLoader;
+
+import com.huiyi.service.TaskAssigneeService;
+
 public class TaskAssigneeListener implements TaskListener, Serializable {
 
+	private static Logger LOGGER = LoggerFactory.getLogger(TaskAssigneeListener.class);
+	
     private static final long serialVersionUID = 478113514195354529L;
 
     private TaskAssigneeService taskAssigneeService;
 
     public void notify(DelegateTask task) {
         try {
-            String taskId = task.getId();
-
-            ApplicationContext context = ContextLoader
-                    .getCurrentWebApplicationContext();
-            taskAssigneeService = (TaskAssigneeService) context
-                    .getBean("TaskAssigneeService");
-            List<String> users = taskAssigneeService.getUserIdsByTaskID(taskId);
+            ApplicationContext context = ContextLoader.getCurrentWebApplicationContext();
+            taskAssigneeService = (TaskAssigneeService) context.getBean("TaskAssigneeService");
+            List<String> users = taskAssigneeService.getUserIdsByTaskID(task.getTaskDefinitionKey());
+            LOGGER.info(task.getExecution().getBusinessKey() +"--"+ task.getTaskDefinitionKey() +"--->:" + users.toString());
             for(String userId: users){
                 task.addCandidateUser(userId);
             }
